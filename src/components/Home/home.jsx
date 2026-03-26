70f01a5534e81e3754c7c303d20e09ea4aaacd1d
NolanTakeuchi <139922542+NolanTakeuchi@users.noreply.github.com>
2026-03-26 14:26:56 -0700

    Fixed times

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
   function calculateTimeLeft() {
     const currentTime = new Date();
-    const targetDate = new Date("2026-03-28");
+    const targetDate = new Date("2026-03-29");
     targetDate.setHours(7, 59, 59); // Set the target time to 11:59 pm
 
     const timeDiff = targetDate.getTime() - currentTime.getTime();
